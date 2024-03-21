128b539f9baf99a50d6fa0de446a03beace1ff10
gabrieltacosta <gabriel.tuka@gmail.com>
2024-03-21 09:44:09 -0300

    ToDo(CRUD) completo

--- todos/models.py
+++ todos/models.py
@@ -1,15 +1,25 @@
 from django.db import models
+from datetime import date
+
 
 # Create your models here.
 class Todo(models.Model):
-    title = models.CharField(verbose_name="Título", max_length=100, null=False, blank=False)
-    created_at = models.DateTimeField(verbose_name="Criado em",auto_now_add=True, null=False, blank=False)
-    deadline = models.DateField(verbose_name="Data de entrega",null=False, blank=False)
-    finished_at = models.DateField(verbose_name="Finalizado em",null=True, blank=True)
+    title = models.CharField(
+        verbose_name="Título", max_length=100, null=False, blank=False
+    )
+    created_at = models.DateTimeField(
+        verbose_name="Criado em", auto_now_add=True, null=False, blank=False
+    )
+    deadline = models.DateField(verbose_name="Data de entrega", null=False, blank=False)
+    finished_at = models.DateField(verbose_name="Finalizado em", null=True, blank=True)
 
     class Meta:
         ordering = ["deadline"]
 
     def __str__(self):
         return self.title
-    
+
+    def mark_has_complete(self):
+        if not self.finished_at:
+            self.finished_at = date.today()
+            self.save()
--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
 from .models import Todo
 
 
@@ -13,3 +13,21 @@ class TodoCreateView(CreateView):
     model = Todo
     fields = ["title", "deadline"]
     success_url = reverse_lazy("todo_list")
+
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ["title", "deadline", "finished_at"]
+    success_url = reverse_lazy("todo_list")
+
+
+class TodoDeleteView(DeleteView):
+    model = Todo
+    success_url = reverse_lazy("todo_list")
+
+
+class TodoCompleteView(View):
+    def get(self, request, pk):
+        todo = get_object_or_404(Todo, pk=pk)
+        todo.mark_has_complete()
+        return redirect("todo_list")
